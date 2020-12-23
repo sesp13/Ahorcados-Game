807d97c58d8cb47b13edffcb81aa1ebce97414f6
Santiago <Santiago@DESKTOP-4EOL7AQ>
2020-12-22 22:44:55 -0500

    ag

--- helpers/setGame.php
+++ helpers/setGame.php
@@ -3,21 +3,53 @@ $espicificaciones = [
     [
         'nombre' => 'Eliecer',
         'imagen' => '',
-        'frase' => 'Con este sancocho ya no hice dieta hasta el otro año'
-    ],
-    [
-        'nombre' => 'Gladys',
-        'imagen' => '',
-        'frase' => 'Si le hablo a este hombre ya no dormimos hasta mañana'
+        'frase' => 'Es febrero y con este sancocho ya no hare dieta hasta el otro año'
     ],
     [
         'nombre' => 'Paola',
         'imagen' => '',
-        'frase' => 'Cómo es posible que ye hayas comido 4 buñuelos, 4, Paola que chancha'
+        'frase' => 'Cómo es posible que te hayas comido 4 buñuelos, 4!, que chancha'
     ],
     [
         'nombre' => 'Juan Roberto',
         'imagen' => '',
-        'frase' => 'Cuidado Alonso con ese parlante que trajo, ya que nos pueden echar acá del parque porque suena demasiado duro'
+        'frase' => 'Cuidado Alonso con ese parlante que trajo, suena demasiado duro'
     ],
+    [
+        'nombre' => 'Esteban',
+        'imagen' => '',
+        'frase' => 'Mama por favor no me eches 3 salchichones en la comida que no me gusta'
+    ],
+    [
+        'nombre' => 'Dora',
+        'imagen' => '',
+        'frase' => 'Esto de administrar no es para todo el mundo, solo lo se yo'
+    ],
+    [
+        'nombre' => 'Humberto',
+        'imagen' => '',
+        'frase' => 'Los hijos de don Roberto no roban, ellos piden'
+    ],
+    [
+        'nombre' => 'Olga',
+        'imagen' => '',
+        'frase' => 'Mijo tomese esta milanta para que pase estas galletas de soda'
+    ],
+    [
+        'nombre' => 'Luz Dary',
+        'imagen' => '',
+        'frase' => 'Hermanita esta segura que no quiere cayos que hace mi mama'
+    ],
+    [
+        'nombre' => 'Sara',
+        'imagen' => '',
+        'frase' => 'Unas papitas de kit kof con malteada de chocolate por favor'
+    ],
+    [
+        'nombre' => 'Nana',
+        'imagen' => '',
+        'frase' => 'Santi venga me va a ayudar con unas cositas de ingles por favor'
+    ],
+    // Frank 
+    // Oscar
 ];
