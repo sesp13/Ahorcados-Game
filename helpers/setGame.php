3244180b6a2d3845e8b1350aa204979d47c7b15c
Santiago <Santiago@DESKTOP-4EOL7AQ>
2020-12-31 20:06:02 -0500

    frases de la familia arteaga

--- helpers/setGame.php
+++ helpers/setGame.php
@@ -1,63 +1,167 @@
 <?php
+// $espicificaciones = [
+//     [
+//         'nombre' => 'Eliecer',
+//         'imagen' => '',
+//         'frase' => 'Es febrero y con este sancocho ya no hare dieta hasta el otro año'
+//     ],
+//     [
+//         'nombre' => 'Paola',
+//         'imagen' => '',
+//         'frase' => 'Cómo es posible que te hayas comido 4 buñuelos, 4!, que chancha'
+//     ],
+//     [
+//         'nombre' => 'Juan Roberto',
+//         'imagen' => '',
+//         'frase' => 'Cuidado Alonso con ese parlante que trajo, suena demasiado duro'
+//     ],
+//     [
+//         'nombre' => 'Esteban',
+//         'imagen' => '',
+//         'frase' => 'Mama por favor no me eches 3 salchichones en la comida que no me gusta'
+//     ],
+//     [
+//         'nombre' => 'Dora',
+//         'imagen' => '',
+//         'frase' => 'Esto de administrar no es para todo el mundo, solo lo se yo'
+//     ],
+//     [
+//         'nombre' => 'Humberto',
+//         'imagen' => '',
+//         'frase' => 'Los hijos de don Roberto no roban, ellos piden'
+//     ],
+//     [
+//         'nombre' => 'Olga',
+//         'imagen' => '',
+//         'frase' => 'Mijo tomese esta milanta para que pase estas galletas de soda'
+//     ],
+//     [
+//         'nombre' => 'Luz Dary',
+//         'imagen' => '',
+//         'frase' => 'Hermanita esta segura que no quiere cayos que hace mi mama'
+//     ],
+//     [
+//         'nombre' => 'Sara',
+//         'imagen' => '',
+//         'frase' => 'Unas papitas de kit kof con malteada de chocolate por favor'
+//     ],
+//     [
+//         'nombre' => 'Nana',
+//         'imagen' => '',
+//         'frase' => 'Yo quiero frijolitos y chicharron para el viernes, ya no quiero mas ensaladas'
+//     ],
+//     [
+//         'nombre' => 'Oscar',
+//         'imagen' => '',
+//         'frase' => 'Como quedaron de bonitas las cabras en el pesebre de mi casa'
+//     ],
+//     [
+//         'nombre' => 'Frank',
+//         'imagen' => '',
+//         'frase' => 'Esta camisa me valio 30 mil pesos en el hueco'
+//     ],
+// ];
+
+//Familia Arteaga
 $espicificaciones = [
     [
-        'nombre' => 'Eliecer',
+        'nombre' => 'Margarita',
         'imagen' => '',
-        'frase' => 'Es febrero y con este sancocho ya no hare dieta hasta el otro año'
+        'frase' => 'Rosario a las 5 de la tarde, Gladys por favor enviar el link'
     ],
     [
-        'nombre' => 'Paola',
+        'nombre' => 'Marina',
         'imagen' => '',
-        'frase' => 'Cómo es posible que te hayas comido 4 buñuelos, 4!, que chancha'
+        'frase' => 'Nelson Por Dios Bendito, la oracion es muy importante'
     ],
     [
-        'nombre' => 'Juan Roberto',
+        'nombre' => 'Sebas',
         'imagen' => '',
-        'frase' => 'Cuidado Alonso con ese parlante que trajo, suena demasiado duro'
+        'frase' => 'Para donde van, para leños? ya enseguida les caigo que yo si puedo'
     ],
     [
-        'nombre' => 'Esteban',
+        'nombre' => 'Nelson',
         'imagen' => '',
-        'frase' => 'Mama por favor no me eches 3 salchichones en la comida que no me gusta'
+        'frase' => 'Yo estoy totalmente de acuerdo con pacho, lo que diga pacho'
     ],
     [
-        'nombre' => 'Dora',
+        'nombre' => 'Otoñiel',
         'imagen' => '',
-        'frase' => 'Esto de administrar no es para todo el mundo, solo lo se yo'
+        'frase' => 'Marina, mira lo que me paso en el ojo'
     ],
     [
-        'nombre' => 'Humberto',
+        'nombre' => 'Mariana',
         'imagen' => '',
-        'frase' => 'Los hijos de don Roberto no roban, ellos piden'
+        'frase' => 'Por que ayudan a los niños y a las niñas no'
     ],
     [
-        'nombre' => 'Olga',
+        'nombre' => 'Mariana',
         'imagen' => '',
-        'frase' => 'Mijo tomese esta milanta para que pase estas galletas de soda'
+        'frase' => 'Por que ayudan a los niños y a las niñas no'
     ],
     [
-        'nombre' => 'Luz Dary',
+        'nombre' => 'Laura',
         'imagen' => '',
-        'frase' => 'Hermanita esta segura que no quiere cayos que hace mi mama'
+        'frase' => 'Mami por favor prenda el microfono'
     ],
     [
-        'nombre' => 'Sara',
+        'nombre' => 'Fernando',
         'imagen' => '',
-        'frase' => 'Unas papitas de kit kof con malteada de chocolate por favor'
+        'frase' => 'Se siente el olor cuando es chorizo'
     ],
     [
-        'nombre' => 'Nana',
+        'nombre' => 'Juan Jose',
         'imagen' => '',
-        'frase' => 'Yo quiero frijolitos y chicharron para el viernes, ya no quiero mas ensaladas'
+        'frase' => 'Me trajo ropa y plata pero no el juego Pixarx'
     ],
     [
-        'nombre' => 'Oscar',
+        'nombre' => 'Pacho',
         'imagen' => '',
-        'frase' => 'Como quedaron de bonitas las cabras en el pesebre de mi casa'
+        'frase' => 'Hola les presento a mi cuñado Nelson, el mas mal casado de la familia'
     ],
     [
-        'nombre' => 'Frank',
+        'nombre' => 'Carlos',
         'imagen' => '',
-        'frase' => 'Esta camisa me valio 30 mil pesos en el hueco'
+        'frase' => 'Es un excelente esposo y compañero'
     ],
+    [
+        'nombre' => 'Samuel',
+        'imagen' => '',
+        'frase' => 'Nos tenemos que ir, y no he jugado nada'
+    ],
+    [
+        'nombre' => 'Julian',
+        'imagen' => '',
+        'frase' => 'Yo creo que esa aplicacion esta hecha con microservicios'
+    ],
+    [
+        'nombre' => 'Marisol',
+        'imagen' => '',
+        'frase' => 'Han visto a minimi y a manchis, se me perdieron'
+    ],
+    [
+        'nombre' => 'Isabel',
+        'imagen' => '',
+        'frase' => 'Sirvale a Alonso de ultimo, que ahi mismo se va por el carro'
+    ],
+    [
+        'nombre' => 'Monica',
+        'imagen' => '',
+        'frase' => 'Las tortas de naranja de monica son las mejores'
+    ],
+    [
+        'nombre' => 'Ana Sofia',
+        'imagen' => '',
+        'frase' => 'Ya aprendi a tocar el piano'
+    ],
+    [
+        'nombre' => 'Alejandra',
+        'imagen' => '',
+        'frase' => 'Es la mejor medica del mundo, amable y comprensiva'
+    ],
+    [
+        'nombre' => 'Yuly',
+        'imagen' => '',
+        'frase' => 'Desde la distancia los sigo queriendo'
+    ]
 ];
